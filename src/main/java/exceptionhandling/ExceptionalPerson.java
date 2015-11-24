a4f3ca9bf0e25523f538a8330d06da0396544274
Richard Jones <iamrichardjones@gmail.com>
2015-11-24 23:16:46 +0000

    Add first cut of documentation to ExceptionalPerson

--- src/main/java/exceptionhandling/ExceptionalPerson.java
+++ src/main/java/exceptionhandling/ExceptionalPerson.java
@@ -1,5 +1,12 @@
 package exceptionhandling;
 
+/**
+ * It can be very tempting for people to throw Exception (or even RuntimeException) when things go wrong.
+ * You should throw and catch the appropriate exceptions.
+ * Often NullPointerException is thrown when there are problems on parameters. This is not strictly correct.
+ * In the example below we are throwing IllegalArgumentException because that is what is wrong. The cause is
+ * a NullPointerException. It's an extra couple of lines of code but for clarity I think it is worth it
+ */
 
 public class ExceptionalPerson {
 
