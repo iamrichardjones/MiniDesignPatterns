b77c90a69da11d6986aa2317a25a173732098e46
Richard Jones <iamrichardjones@gmail.com>
2015-11-26 16:25:01 +0000

    Add more comments to tests

--- src/test/java/numbercounting/DayCounterTest.java
+++ src/test/java/numbercounting/DayCounterTest.java
@@ -7,9 +7,10 @@ public class DayCounterTest {
 
     @Test
     public void test_Count() {
-        assertEquals(31, DayCounter.getTotal(1));
-        assertEquals(59, DayCounter.getTotal(2));
-        assertEquals(365, DayCounter.getTotal(12));
+        assertEquals("Jan (31)", 31, DayCounter.getTotal(1));
+        assertEquals("Jan (31) + Feb (28)", 59, DayCounter.getTotal(2));
+        assertEquals("Jan (31) + Feb (28) + Mar  (31)+ Apr (30) + May (31) + Jun (30) + " +
+                     "Jul (31) + Aug (31) + Sep (30) + Oct + Nov (30) + Dec (31)",
+                365, DayCounter.getTotal(12));
     }
-
 }
