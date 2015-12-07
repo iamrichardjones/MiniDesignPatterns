61cf528551d4b0e8cd12f521f6be33a56a933d8e
Richard Jones <iamrichardjones@gmail.com>
2015-12-07 19:19:29 +0000

    Add pair and triplet class

---
+++ src/main/java/pairandtriplet/Pair.java
@@ -0,0 +1,15 @@
+package pairandtriplet;
+
+/**
+ * Util class
+ */
+public class Pair<K, V> {
+
+    public final K one;
+    public final V two;
+
+    public Pair(K one, V two) {
+        this.one = one;
+        this.two = two;
+    }
+}
---
+++ src/main/java/pairandtriplet/Triplet.java
@@ -0,0 +1,17 @@
+package pairandtriplet;
+
+/**
+ * Util class
+ */
+public class Triplet<K, U, V> {
+
+    public final K one;
+    public final U two;
+    public final V three;
+
+    public Triplet(K one, U two, V three) {
+        this.one = one;
+        this.two = two;
+        this.three = three;
+    }
+}
