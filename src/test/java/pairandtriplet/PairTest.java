db2de28eba43e116c244c89fdab1ff0030bf77a2
Richard Jones <iamrichardjones@gmail.com>
2015-12-11 09:01:39 +0000

    white space to PairTest

--- src/test/java/pairandtriplet/PairTest.java
+++ src/test/java/pairandtriplet/PairTest.java
@@ -9,10 +9,13 @@ public class PairTest {
     @Test
     public void test_Pair() {
         Pair<String, Integer> pair = new Pair<String, Integer>("a", 1);
+
         assertEquals("java.lang.String", pair.getOne().getClass().getName());
         assertEquals("java.lang.Integer", pair.getTwo().getClass().getName());
+
         assertEquals("a", pair.getOne());
         assertEquals(new Integer(1), pair.getTwo());
+
         assertEquals("a", pair.one);
         assertEquals(new Integer(1), pair.two);
     }
