09a2e6862554dce1a9d32d076b44fa51405e256e
Richard Jones <iamrichardjones@gmail.com>
2015-12-11 09:04:12 +0000

    update comment for CaseStatementRunner

--- src/main/java/casestatement/CaseStatementRunner.java
+++ src/main/java/casestatement/CaseStatementRunner.java
@@ -1,10 +1,13 @@
 package casestatement;
 
 /**
- * Most often case statements have a list of valid inputs then a default section. If there is a programmer error
- * and the default section is called (and there is nothing entered in that part) then it can cause hard to find
- * bugs later on.
- * I recommend throwing an exception rather than having a blank default statement
+ * This mini-design pattern helps find programmer errors when the default section of a case statement is executed.
+ * Most often case statements have a list of valid inputs then a default section. If the code/reviewer missing one
+ * section on the case statement then the default section is called (and there is nothing entered in that part)
+ * This can cause hard to find bugs later on.
+ *
+ * I recommend throwing an exception rather than having a blank default statement. If the case statement can be
+ * extended then this is a better pattern than relying on default
  */
 public class CaseStatementRunner {
 
