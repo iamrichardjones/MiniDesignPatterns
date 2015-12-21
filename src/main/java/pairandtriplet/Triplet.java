2fa0b8eeb154735162bff49c8590624dd54b8346
Richard Jones <iamrichardjones@gmail.com>
2015-12-21 20:10:28 +0000

    improve comment in Triplet.java

--- src/main/java/pairandtriplet/Triplet.java
+++ src/main/java/pairandtriplet/Triplet.java
@@ -3,13 +3,13 @@ package pairandtriplet;
 /**
  * Util class used as a mini-design pattern.
  *
- * The object of this class is pure laziness. You may have method that you want to return two values from. Do you
- * really want to return a new object or even worse an array. With this class you can return any values you want
- * in a simple manor. The one anti-pattern I see here is the fact that the getters and the paremeters are not named
- * in a more coder friendly way. They are simple one, two, three etc.
+ * The purpose of this class is pure laziness. You may have method that you want to return three values from. Do you
+ * really want to return a new object or even worse an array? With this class you can return any values you want
+ * in a simple manor. The one anti-pattern I see here is the fact that the getters and the parameters are not named
+ * in a more "coder friendly" way. They are simple one, two, and three.
  *
- * Here I am making the parameters public so the coder can access them in a simpler form. It may go against convention but
- * it they are final so I feel this is ok (although feel free to disagree ;)
+ * Here I am making the parameters public so the coder can access them in a simpler form. It may go against convention
+ * but they are final so I feel this is ok (although feel free to disagree ;)
  */
 public class Triplet<K, U, V> {
 
