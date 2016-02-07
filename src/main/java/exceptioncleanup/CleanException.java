324b909665b1d95f40e54fcc85e94ca185b95e7a
Richard Jones <iamrichardjones@gmail.com>
2016-02-07 21:39:20 +0000

    added comment

--- src/main/java/exceptioncleanup/CleanException.java
+++ src/main/java/exceptioncleanup/CleanException.java
@@ -1,5 +1,11 @@
 package exceptioncleanup;
 
+/**
+ * Here we have a class that assigns a value then throws an exception. The mini-pattern is best seen in
+ * the unit test. Here we verify that an exception is thrown but ensure that we check the values that
+ * are left over after this exception. It is possible for the caller to catch the exception and still use
+ * the object that is instantiated. Therefore we should always test for this.
+ */
 public class CleanException {
 
     private Integer value;
